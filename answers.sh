bf623bb0d2b9ea21f9ca6fee01152d62016fd6ed
Vasque31 <70483936+Vasque31@users.noreply.github.com>
2020-09-14 20:37:07 -0400

    Update answers.sh

--- answers.sh
+++ answers.sh
@@ -4,42 +4,42 @@
 cd ORIGIN
 
 # Task 1 
-
+mv Homework/MA165/HW2.txt Homework/MA165/HW1.txt
 
 # Task 2
-
+rm -r Memes/SpongeBob/money.jpeg
 
 # Task 3
-
+mv Memes/Star Wars/fafsa.jpg Memes/SpongeBob/fafsa.jpg
 
 # Task 4
-
+rm -r Homework/CS191
 
 # Task 5
-
+mkdir Programs/C++
 
 # Task 6
-
+mv Programs/Age.cpp Programs/C++/Age.cpp
 
 # Task 7
-
+cp Programs/Java Homework/CS180/Project_1
 
 # Task 8
-
-
+mv Programs/*.C Programs/C
 # Task 9
-
+mv Programs/*.py Programs/Python 3
 
 # Task 10
-
+touch Homework/POL141/Essay.txt
 
 # Task 11
-
+ls ..
 
 # Task 12
 : '
 Type your vim/nano customizations below:
+set fill 30
+set autoindent
+set morespace
 
-
-
-'
+‘
